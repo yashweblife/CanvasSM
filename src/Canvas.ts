b0bdf123be0187e8cdb5f90fbdda0c96fb55de0e
yashweblife <yashweblife20@gmail.com>
2022-11-22 18:29:05 -0600

    added more features

--- src/Canvas.ts
+++ src/Canvas.ts
@@ -1,8 +1,12 @@
+import { CircleInterface } from "./interfaces";
 import { Vector } from "./Vector";
+
+
 
 export class Canvas {
   private size: Vector = new Vector(200, 200);
   private dom: HTMLCanvasElement = document.createElement("canvas");
+  private c: CanvasRenderingContext2D = this.dom.getContext("2d")!;
   constructor(parent: HTMLElement = document.body) {
     parent.append(this.dom);
     this.recalib();
@@ -14,5 +18,38 @@ export class Canvas {
   public setSize = (val: Vector = new Vector(200, 200)) => {
     this.size = val;
     this.recalib();
+  };
+  public bound = () => {
+    const data:DOMRect = this.dom.getBoundingClientRect();
+    return {
+      top: data.top,
+      bottom: data.bottom,
+      left: data.left,
+      right: data.right,
+    };
+  };
+  public circle = (data: CircleInterface) => {
+    this.c.beginPath();
+    this.c.arc(data.pos.x, data.pos.y, data.size, 0, Math.PI * 2, false);
+    this.c.closePath();
+    if (data.fill) {
+      this.c.fillStyle = data.fillColor ? data.fillColor : "red";
+      this.c.fill();
+    }
+    if (data.stroke) {
+      this.c.strokeStyle = data.strokeColor ? data.strokeColor : "black";
+      this.c.stroke();
+    }
+  };
+  public line = (a:Vector, b:Vector, color:string = "black")=>{
+    this.c.beginPath()
+    this.c.moveTo(a.x, a.y)
+    this.c.lineTo(b.x,b.y)
+    this.c.closePath()
+    this.c.strokeStyle = color;
+    this.c.stroke()
+  }
+  public clear = () => {
+    this.c.clearRect(0, 0, this.size.x, this.size.y);
   };
 }
