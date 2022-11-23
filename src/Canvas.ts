020b41e888bb0369eafb6d5504cb8d6bff89c349
yashweblife <yashweblife20@gmail.com>
2022-11-22 18:50:29 -0600

    formatted

--- src/Canvas.ts
+++ src/Canvas.ts
@@ -1,12 +1,10 @@
-import { CircleInterface } from "./interfaces";
-import { Vector } from "./Vector";
-
-
+import { CircleInterface } from './interfaces';
+import { Vector } from './Vector';
 
 export class Canvas {
   private size: Vector = new Vector(200, 200);
-  private dom: HTMLCanvasElement = document.createElement("canvas");
-  private c: CanvasRenderingContext2D = this.dom.getContext("2d")!;
+  private dom: HTMLCanvasElement = document.createElement('canvas');
+  private c: CanvasRenderingContext2D = this.dom.getContext('2d')!;
   constructor(parent: HTMLElement = document.body) {
     parent.append(this.dom);
     this.recalib();
@@ -20,7 +18,7 @@ export class Canvas {
     this.recalib();
   };
   public bound = () => {
-    const data:DOMRect = this.dom.getBoundingClientRect();
+    const data: DOMRect = this.dom.getBoundingClientRect();
     return {
       top: data.top,
       bottom: data.bottom,
@@ -33,22 +31,22 @@ export class Canvas {
     this.c.arc(data.pos.x, data.pos.y, data.size, 0, Math.PI * 2, false);
     this.c.closePath();
     if (data.fill) {
-      this.c.fillStyle = data.fillColor ? data.fillColor : "red";
+      this.c.fillStyle = data.fillColor ? data.fillColor : 'red';
       this.c.fill();
     }
     if (data.stroke) {
-      this.c.strokeStyle = data.strokeColor ? data.strokeColor : "black";
+      this.c.strokeStyle = data.strokeColor ? data.strokeColor : 'black';
       this.c.stroke();
     }
   };
-  public line = (a:Vector, b:Vector, color:string = "black")=>{
-    this.c.beginPath()
-    this.c.moveTo(a.x, a.y)
-    this.c.lineTo(b.x,b.y)
-    this.c.closePath()
+  public line = (a: Vector, b: Vector, color: string = 'black') => {
+    this.c.beginPath();
+    this.c.moveTo(a.x, a.y);
+    this.c.lineTo(b.x, b.y);
+    this.c.closePath();
     this.c.strokeStyle = color;
-    this.c.stroke()
-  }
+    this.c.stroke();
+  };
   public clear = () => {
     this.c.clearRect(0, 0, this.size.x, this.size.y);
   };
