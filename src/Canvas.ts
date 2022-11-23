a0b1e042f89358d4527fcca5a91ed60decb9681c
yashweblife <yashweblife20@gmail.com>
2022-11-22 21:48:06 -0600

    added new canvas

--- src/Canvas.ts
+++ src/Canvas.ts
@@ -1,53 +1,110 @@
-import { CircleInterface } from './interfaces';
-import { Vector } from './Vector';
+import { CircleInterface, RectInterface } from "./interfaces";
+import { Vector } from "./Vector";
 
 export class Canvas {
-  private size: Vector = new Vector(200, 200);
-  private dom: HTMLCanvasElement = document.createElement('canvas');
-  private c: CanvasRenderingContext2D = this.dom.getContext('2d')!;
-  constructor(parent: HTMLElement = document.body) {
-    parent.append(this.dom);
-    this.recalib();
+  public dom: HTMLCanvasElement;
+  public ctx: CanvasRenderingContext2D;
+  public width: number;
+  public height: number;
+  constructor(
+    canvas: HTMLCanvasElement = document.createElement("canvas"),
+    width = 300,
+    height = 300
+  ) {
+    this.dom = canvas;
+    this.dom.width = width;
+    this.dom.height = height;
+    this.ctx = this.dom.getContext("2d")!;
+    this.width = this.dom.width;
+    this.height = this.dom.height;
   }
-  private recalib = () => {
-    this.dom.width = this.size.x;
-    this.dom.height = this.size.y;
+  public setSize = (w: number, h: number) => {
+    this.dom.width = w;
+    this.dom.height = h;
+    this.width = w;
+    this.height = h;
   };
-  public setSize = (val: Vector = new Vector(200, 200)) => {
-    this.size = val;
-    this.recalib();
-  };
-  public bound = () => {
-    const data: DOMRect = this.dom.getBoundingClientRect();
-    return {
-      top: data.top,
-      bottom: data.bottom,
-      left: data.left,
-      right: data.right,
-    };
-  };
-  public circle = (data: CircleInterface) => {
-    this.c.beginPath();
-    this.c.arc(data.pos.x, data.pos.y, data.size, 0, Math.PI * 2, false);
-    this.c.closePath();
-    if (data.fill) {
-      this.c.fillStyle = data.fillColor ? data.fillColor : 'red';
-      this.c.fill();
+  public circle = ({
+    pos = new Vector(0, 0),
+    size = 5,
+    fillColor = "red",
+    strokeColor = "black",
+    stroke = false,
+    fill = true,
+  }: CircleInterface) => {
+    this.ctx.beginPath();
+    this.ctx.fillStyle = fillColor;
+    this.ctx.strokeStyle = strokeColor;
+    this.ctx.arc(pos.x, pos.y, size, 0, Math.PI * 2, false);
+    this.ctx.closePath();
+    if (fill) {
+      this.ctx.fill();
     }
-    if (data.stroke) {
-      this.c.strokeStyle = data.strokeColor ? data.strokeColor : 'black';
-      this.c.stroke();
+    if (stroke) {
+      this.ctx.stroke();
     }
   };
-  public line = (a: Vector, b: Vector, color: string = 'black') => {
-    this.c.beginPath();
-    this.c.moveTo(a.x, a.y);
-    this.c.lineTo(b.x, b.y);
-    this.c.closePath();
-    this.c.strokeStyle = color;
-    this.c.stroke();
+  public rect = ({
+    pos = new Vector(0, 0),
+    size = new Vector(100, 100),
+    fillColor = "red",
+    strokeColor = "black",
+    stroke = false,
+    fill = true,
+    angle = 0,
+  }: RectInterface) => {
+    this.ctx.save()
+    this.ctx.rotate(angle)
+    this.ctx.beginPath();
+    this.ctx.fillStyle = fillColor;
+    this.ctx.strokeStyle = strokeColor;
+    this.ctx.rect(pos.x, pos.y, size.x, size.y);
+    this.ctx.closePath();
+    if (fill === true) this.ctx.fill();
+    if (stroke === true) this.ctx.stroke();
+    this.ctx.restore();
+  };
+  public line = (v1: Vector, v2: Vector, color:string = 'rgba(0,0,0,0.5)') => {
+    this.ctx.beginPath();
+    this.ctx.strokeStyle = color;
+    this.ctx.moveTo(v1.x, v1.y);
+    this.ctx.lineTo(v2.x, v2.y);
+    this.ctx.closePath();
+    this.ctx.stroke();
+  };
+  //public reverseRect = ({pos, size, fillColor, strokeColor, stroke, fill, angle})=>{}
+  public fillPath = (vec:Vector[],color:string = "red") => {
+    this.ctx.beginPath()
+    this.ctx.moveTo(vec[0].x, vec[0].y)
+    for(var i=1;i<vec.length;i++){
+      const v2 = vec[i]
+      this.ctx.lineTo(v2.x, v2.y)
+    }
+    this.ctx.closePath()
+    this.ctx.fillStyle = color;
+    this.ctx.fill()
   };
   public clear = () => {
-    this.c.clearRect(0, 0, this.size.x, this.size.y);
+    this.ctx.clearRect(0, 0, this.width, this.height);
   };
+  public fadeClear = () => {
+    this.ctx.fillStyle = "rgba(255,255,255,0.01)";
+    this.ctx.rect(0, 0, this.width, this.height);
+    this.ctx.fill();
+  };
+  public drawImage = (img:HTMLVideoElement)=>{
+    this.ctx.drawImage(img, 0, 0, this.width, this.height)
+  }
+  public getImageData = ():ImageData=>{
+    let img:ImageData = this.ctx.getImageData(0,0,this.width, this.height,{willReadFrequently:true} as ImageDataSettings)
+    return(img)
+  }
+  public pixel = (pos:Vector, color:Vector)=>{
+    this.ctx.beginPath()
+    this.ctx.fillStyle = `rgb(${color.x}, ${color.y},${color.z})`
+    this.ctx.rect(pos.x,pos.y, 1,1)
+    this.ctx.closePath()
+    this.ctx.fill()
+
+  }
 }
