e6ede17e8e263f4ad1ce6125256d19deadc30a3e
yashweblife <yashweblife20@gmail.com>
2022-11-25 16:11:20 -0600

    added more functions

--- src/Polar.ts
+++ src/Polar.ts
@@ -1,25 +1,36 @@
-import { Vector } from "./Vector";
+import { Vector } from './Vector';
 
-export class Polar{
-    public r:number=0;
-    public theta:number=0;
-    constructor(r:number=0,theta:number=0){
-        this.r = r;
-        this.theta=theta
-    }
-    public add = (val:Polar)=>{
-        this.r+=val.r
-        this.theta+=val.theta
-    }
-    public scalar = (val:number)=>{
-        this.r*=val
-    }
-    public toVector = ():Vector=>{
-        return(new Vector(this.r*Math.cos(this.theta), this.r*Math.sin(this.theta)))
-    }
-    public static fromVector = (val:Vector):Polar=>{
-        const r = val.mag;
-        const theta = Math.atan2(val.y,val.x)
-        return(new Polar(r,theta))
-    }
+export class Polar {
+  public r: number = 0;
+  public theta: number = 0;
+  constructor(r: number = 0, theta: number = 0) {
+    this.r = r;
+    this.theta = theta;
+  }
+  public add = (val: Polar) => {
+    this.r += val.r;
+    this.theta += val.theta;
+  };
+  public scalar = (val: number) => {
+    this.r *= val;
+  };
+  public toVector = (): Vector => {
+    return new Vector(this.r * Math.cos(this.theta), this.r * Math.sin(this.theta));
+  };
+  public static fromVector = (val: Vector): Polar => {
+    const r = val.mag;
+    const theta = Math.atan2(val.y, val.x);
+    return new Polar(r, theta);
+  };
+  public normalize = () => {
+    const x = Math.cos(this.theta);
+    const y = Math.sin(this.theta);
+    this.r = Math.atan2(y, x);
+  };
+  public rand = () => {
+    return new Polar(Math.random(), Math.random());
+  };
+  public randSigned = () => {
+    return new Polar(Math.random() - 0.5, Math.random() - 0.5);
+  };
 }
