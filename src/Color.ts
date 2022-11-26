c5b593d07d8a36f24e027490e6a2a26714e50bd9
yashweblife <yashweblife20@gmail.com>
2022-11-25 19:22:23 -0600

    Added more functions

--- src/Color.ts
+++ src/Color.ts
@@ -1,3 +1,6 @@
+/**
+ * # Color
+ */
 export class Color {
   public r: number = 0;
   public g: number = 0;
@@ -29,6 +32,55 @@ export class Color {
     this.g *= val;
     this.b *= val;
     this.recalib();
+  };
+  public maximize = () => {
+    if (this.r > this.g && this.r > this.b) {
+      this.r = 1;
+      this.g = 0;
+      this.b = 0;
+    } else if (this.g > this.r && this.g > this.b) {
+      this.r = 0;
+      this.g = 1;
+      this.b = 0;
+    } else if (this.b > this.r && this.b > this.g) {
+      this.r = 0;
+      this.g = 0;
+      this.b = 1;
+    }
+    this.scalar(255);
+  };
+  public minimize = () => {
+    if (this.r < this.g && this.r < this.b) {
+      this.r = 1;
+      this.g = 0;
+      this.b = 0;
+    } else if (this.g < this.r && this.g < this.b) {
+      this.r = 0;
+      this.g = 1;
+      this.b = 0;
+    } else if (this.b < this.r && this.b < this.g) {
+      this.r = 0;
+      this.g = 0;
+      this.b = 1;
+    }
+    this.scalar(255);
+  };
+  public complement = () => {
+    const r = 0.5 + (0.5 - this.r / 255) * 255;
+    const g = 0.5 + (0.5 - this.g / 255) * 255;
+    const b = 0.5 + (0.5 - this.b / 255) * 255;
+    return new Color(Math.round(r), Math.round(g), Math.round(b));
+  };
+  public generateSimilar = (size: number): Color[] => {
+    const output: Color[] = [];
+    for (var i = 0; i < size; i++) {
+      const mux = i / 10;
+      const r = this.r / 255 + mux;
+      const g = this.g / 255 + mux;
+      const b = this.b / 255 + mux;
+      output.push(new Color(r * 255, g * 255, b * 255));
+    }
+    return output;
   };
   public getColor = () => {
     return `rgba(${this.r}, ${this.g}, ${this.b}, ${this.a})`;
