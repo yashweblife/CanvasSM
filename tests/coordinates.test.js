803743b4006f4dd2e6f9a096736c4c9821203905
yashweblife <yashweblife20@gmail.com>
2022-11-28 14:26:01 -0600

    deleted

--- tests/coordinates.test.js
+++
@@ -1,7 +0,0 @@
-import {Spherical} from "./src/Spherical"
-    
-test('Gets distance between 2 sphers',()=>{
-    const s1 = new Spherical(1)
-    const s2 = new Spherical(10)
-    expect(s1.dist(s2)).toBe(9)
-})
