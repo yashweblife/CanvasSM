260841234592c86071fe18091ff58f4541f6fad0
yashweblife <yashweblife20@gmail.com>
2022-11-30 04:22:41 -0600

    Organized imports

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,15 @@
-import { Vector } from './Vector';
-import { Polar } from './Polar';
-import { Spherical } from './Spherical';
 import { Canvas } from './Canvas';
-import { Mouse } from './Mouse';
-import { PhysicsObject } from './PhysicsObject';
 import { Color } from './Color';
 import {
-  CircleInterface,
-  PolarInterface,
-  ColorInterface,
-  LineInterface,
-  VectorInterface,
-  RectInterface,
+  CircleInterface, ColorInterface,
+  LineInterface, PolarInterface, RectInterface, VectorInterface
 } from './interfaces';
+import { Mouse } from './Mouse';
+import { PhysicsObject } from './PhysicsObject';
+import { Polar } from './Polar';
+import { Spherical } from './Spherical';
+import { Vector } from './Vector';
 export { Vector, Polar, Spherical, Canvas, Mouse, PhysicsObject, Color };
-
 export { CircleInterface, PolarInterface, ColorInterface, LineInterface, VectorInterface, RectInterface };
+
+
