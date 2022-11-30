41810222ae8f97b9ba93b5c940b3aec3bcbed8ac
yashweblife <yashweblife20@gmail.com>
2022-11-30 06:17:03 -0600

    added wall

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
+import { Ball } from './Ball';
 import { Canvas } from './Canvas';
 import { Color } from './Color';
 import {
-  CircleInterface, ColorInterface,
-  LineInterface, PolarInterface, RectInterface, VectorInterface
+  CircleInterface,
+  ColorInterface,
+  LineInterface,
+  PolarInterface,
+  RectInterface,
+  VectorInterface
 } from './interfaces';
 import { Mouse } from './Mouse';
 import { PhysicsObject } from './PhysicsObject';
 import { Polar } from './Polar';
 import { Spherical } from './Spherical';
 import { Vector } from './Vector';
-export { Vector, Polar, Spherical, Canvas, Mouse, PhysicsObject, Color };
+import { Wall } from './Wall';
+export { Vector, Polar, Spherical, Canvas, Wall, Mouse, PhysicsObject, Color, Ball };
 export { CircleInterface, PolarInterface, ColorInterface, LineInterface, VectorInterface, RectInterface };
-
 
